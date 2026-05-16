9672d31028ca90ab0aa4405ec7d2444f0eac4a3f
E11a-001 <ellachicookie00@gmail.com>
2026-05-16 15:12:31 +0100

    Add core library modules

---
+++ src/lib/forms/fill-fields.ts
@@ -0,0 +1,6 @@
+export function fillField(element: HTMLInputElement | HTMLTextAreaElement, value: string) {
+  element.focus();
+  element.value = value;
+  element.dispatchEvent(new Event('input', { bubbles: true }));
+  element.dispatchEvent(new Event('change', { bubbles: true }));
+}
